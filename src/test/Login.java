61a95b3a7f048e6021289f6016906b72e8204fe9
nijat1985 <57869658+nijat1985@users.noreply.github.com>
2020-04-09 22:49:42 -0400

    Update Login.java

--- src/test/Login.java
+++ src/test/Login.java
@@ -2,34 +2,7 @@ package test;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-
-        //This line is from IntelliJ
-
-        //This one is from github
-
-        System.out.println("I am in Luba Branch right now");
-        System.out.println("I am in Chaouki Branch right now");
-        
-
-        System.out.println("I am coming from the Moon");
-
-
-        System.out.println("I am going back to the Moon");
-
-
-        System.out.println("I should see it only in Luba");
-        System.out.println("I should see it only in master");
-
-        
-        
-        
-        System.out.println("Something funny)))))))))))))))))))))))))))))))))))))))))))");
-
-
-
-        System.out.println("jhlsdfbglkdbvldkbalkbgalksbgakbgalkbgakbglabglakjbfskldb");
+//fgghjklsbvnsdajklcbnvjcx bjcxbnjcnxsjxcsjkcnxsjkcxmcxmsxc
 
 
     }
